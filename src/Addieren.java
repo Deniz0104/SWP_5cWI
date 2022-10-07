67cde1ab31052a96735e0cac3b0e404f9b1dbc70
Deniz <deniz.karakoc0104@gmail.com>
2022-10-07 10:01:29 +0200

    addieren

--- src/Addieren.java
+++ src/Addieren.java
@@ -1,23 +1,32 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Addieren {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int summand1 = scanner.nextInt();
-        LinkedList<Integer> list = new LinkedList<Integer>();
-        while (summand1 > 0) {
-            list.push( summand1 % 10 );
-            summand1 = summand1 / 10;
-        }
+        System.out.println("First number: ");
+        String summand1 = scanner.nextLine();
 
-        while (!list.isEmpty()) {
-            System.out.print(list.pop() + " ");
-        }
+        System.out.println("Second number: ");
+        String summand2 = scanner.nextLine();
 
+
+        int nextPosition = 0;
+        String summe = "";
+        for (int i = summand1.length() - 1; i >= 0; i--) {
+            int summePosition = Integer.parseInt(summand1.substring(i, i + 1)) + Integer.parseInt(summand2.substring(i, i + 1)) + nextPosition;
+            nextPosition = 0;
+            if (summePosition >= 10) {
+                nextPosition = 1;
+            }
+            summe = summePosition + summe;
+        }
+        System.out.println(summand1);
+        System.out.println(summand2 + " + ");
+        System.out.println("--------------");
+        System.out.println(summe);
 
     }
 }
+
+
